4f074763c317da0db203b1a00756d557b4ae345b
WiktorT444 <wiktor.tupiak@gmail.com>
2025-01-18 14:01:59 +0100

    basic_commands 0.1.1
    Added help command
    Changed hello command

--- main_censored.py
+++ main_censored.py
@@ -12,6 +12,11 @@ async def on_ready():
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send("Hello")
+    await ctx.send("Hello;)")
+
+@bot.command()
+async def help(ctx):
+    await ctx.send("All commands:\n$help:Show all the commands.\n$hello:I just say hello:).\n$plus:Adds two numbers and i write the answer(not added yet).\n$minus:It dose exactly what you think(not added yet)\n$multiply:It wasn't added yet")
+
 
 bot.run(">INSERT TOKEN HERE<")
