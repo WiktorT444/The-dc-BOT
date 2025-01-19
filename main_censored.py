80b47b8393e55edc07804bd0a5c85ad2b8a5fa21
WiktorT444 <wiktor.tupiak@gmail.com>
2025-01-19 21:35:01 +0100

    math_commands 1.1.1
    Added $minus command
    Changed $helping command

--- main_censored.py
+++ main_censored.py
@@ -16,11 +16,18 @@ async def hello(ctx):
     await ctx.send(f"Hello;)I am {bot.user} use $help to see all commands.")
 #command for $help1
 @bot.command()
-async def help1(ctx):
-    await ctx.send("All commands:\n$help:Show all the commands.\n$hello:I just say hello:).\n$plus:Adds two numbers and i write the answer(write your numbers after $plus also with commands like $minus and $multiply).\n$minus:It dose exactly what you think(not added yet)\n$multiply:It wasn't added yet")
+async def helping(ctx, page = 1):
+    if page == 1:
+        await ctx.send("All commands(page 1):\n$helping:Show all the commands.\n$hello:I just say hello:).\n$plus (number 1) (number2):Adds two numbers and i write the answer\n(write your numbers after $plus also with commands like $minus and $multiply).\nType helping 2 too see more")
+    elif page == 2:
+        await ctx.send("All commands(page 2):\n$minus (number 1) (number2):It dose exactly what you think\n$multiply (number 1) (number2):It wasn't added yet")
 #command for $plus
 @bot.command()
 async def plus(ctx, l1 = 0, l2 = 0):
     await ctx.send(f"It equals:{l1 + l2}.")
+#command for $minus
+@bot.command()
+async def minus(ctx, l1 = 1, l2 = 0):
+    await ctx.send(f"It equals:{l1 - l2}")
 
 bot.run(">INSERT TOKEN HERE<")
