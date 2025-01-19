53df0f4d1c601443f41353704a43bdcdfeaa1207
WiktorT444 <wiktor.tupiak@gmail.com>
2025-01-19 09:17:06 +0100

    basic_commands  0.1.1 bug fix
    fixed help command
    now you need to type "$help1"
    comments:)

--- main_censored.py
+++ main_censored.py
@@ -1,21 +1,22 @@
 import discord
 from discord.ext import commands
-
+#intents for bot
 intents = discord.Intents.default()
 intents.message_content = True
-
+#var for bot and command prefix    v   You can change it here
 bot = commands.Bot(command_prefix='$', intents=intents)
-
+#Who you logged in as?
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
-
+#commands
+#command for $hello
 @bot.command()
 async def hello(ctx):
-    await ctx.send("Hello;)")
-
+    await ctx.send(f"Hello;)I am {bot.user} use $help to see all commands.")
+#command for $help1
 @bot.command()
-async def help(ctx):
+async def help1(ctx):
     await ctx.send("All commands:\n$help:Show all the commands.\n$hello:I just say hello:).\n$plus:Adds two numbers and i write the answer(not added yet).\n$minus:It dose exactly what you think(not added yet)\n$multiply:It wasn't added yet")
 
 
